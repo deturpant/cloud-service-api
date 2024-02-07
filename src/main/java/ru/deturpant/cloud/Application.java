b2e02dfc6d5669ca7a77ac21fa59430f04e0c95b
deturpant <vlad3032.kozlov@yandex.ru>
2024-02-07 18:15:21 +0300

    Application class init

---
+++ src/main/java/ru/deturpant/cloud/Application.java
@@ -0,0 +1,10 @@
+package ru.deturpant.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}

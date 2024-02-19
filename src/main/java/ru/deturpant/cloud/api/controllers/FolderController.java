fd6a552d6af2bfe3168812c4bbdee708cc1f4fbb
deturpant <vlad3032.kozlov@yandex.ru>
2024-02-19 15:43:58 +0300

    implements FolderController

---
+++ src/main/java/ru/deturpant/cloud/api/controllers/FolderController.java
@@ -0,0 +1,63 @@
+package ru.deturpant.cloud.api.controllers;
+
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+import ru.deturpant.cloud.api.dto.FolderDto;
+import ru.deturpant.cloud.api.exceptions.BadRequestException;
+import ru.deturpant.cloud.api.exceptions.NotFoundException;
+import ru.deturpant.cloud.api.factories.FolderDtoFactory;
+import ru.deturpant.cloud.store.entities.FolderEntity;
+import ru.deturpant.cloud.store.entities.UserEntity;
+import ru.deturpant.cloud.store.repositories.FolderRepository;
+import ru.deturpant.cloud.store.repositories.UserRepository;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@Transactional
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class FolderController {
+    FolderDtoFactory folderDtoFactory;
+    FolderRepository folderRepository;
+    UserRepository userRepository;
+    static final String CREATE_FOLDER = "/api/folders";
+    static final String GET_USER_ROOT_FOLDER = "/api/users/{user_id}/root-folder";
+
+    @PostMapping(CREATE_FOLDER)
+    public FolderDto createFolder(
+            @RequestParam(required = true) String name,
+            @RequestParam(required = true) Long owner_id,
+            @RequestParam(required = false) Long root_folder_id
+    ) {
+        UserEntity user = userRepository.findById(owner_id)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+        FolderEntity rootFolder = null;
+        if (root_folder_id != null) {
+            rootFolder = folderRepository.findById(root_folder_id)
+                    .orElseThrow(() -> new BadRequestException("Root folder with id " + root_folder_id + " not found."));
+        }
+        FolderEntity folderEntity = FolderEntity.builder()
+                .root_folder(rootFolder)
+                .owner(user)
+                .name(name)
+                .createdAt(Instant.now())
+                .modifiedAt(Instant.now())
+                .build();
+        return folderDtoFactory.makeFolderDto(folderEntity);
+    }
+    @GetMapping(GET_USER_ROOT_FOLDER)
+    public FolderDto getUserRootFolder(@PathVariable("user_id") Long userId) {
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+
+        FolderEntity rootFolder = folderRepository.findByOwnerAndRootFolderIsNull(user)
+                .orElseThrow(() -> new NotFoundException("Root folder not found for user with id " + userId));
+
+        return folderDtoFactory.makeFolderDto(rootFolder);
+    }
+}
--- src/main/java/ru/deturpant/cloud/store/repositories/FolderRepository.java
+++ src/main/java/ru/deturpant/cloud/store/repositories/FolderRepository.java
@@ -2,7 +2,11 @@ package ru.deturpant.cloud.store.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.deturpant.cloud.store.entities.FolderEntity;
+import ru.deturpant.cloud.store.entities.UserEntity;
+
+import java.util.Optional;
 
 public interface FolderRepository extends JpaRepository<FolderEntity, Long> {
+    Optional<FolderEntity> findByOwnerAndRootFolderIsNull(UserEntity owner);
 
 }

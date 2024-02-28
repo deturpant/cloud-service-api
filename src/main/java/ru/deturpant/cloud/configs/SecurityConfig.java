1cd14ae6386561753eae4c73b51fd1b6abf77bd2
deturpant <vlad3032.kozlov@yandex.ru>
2024-02-28 21:47:35 +0300

    installed CORS rules

--- src/main/java/ru/deturpant/cloud/configs/SecurityConfig.java
+++ src/main/java/ru/deturpant/cloud/configs/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.deturpant.cloud.api;
+package ru.deturpant.cloud.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import ru.deturpant.cloud.api.jwt.JwtTokenFilter;
 import ru.deturpant.cloud.api.jwt.JwtTokenProvider;
 
@@ -46,5 +49,17 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
